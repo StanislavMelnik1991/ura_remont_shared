7eaf0aeb61d5667f01102866701625663f629358
StanislavMelnik1991 <StanislavMelnik@gmail.com>
2024-03-21 22:24:59 +0300

    update type routes

--- routes/admin/type/index.ts
+++ routes/admin/type/index.ts
@@ -1,14 +1,17 @@
 import { BaseRouter } from '../../baseRouter';
 import { CreateType } from './create';
 import { CurrentRoute } from './current';
+import { GetAllRoute } from './getAll';
 
 export class TypeRouter extends BaseRouter {
   create: CreateType;
   current: CurrentRoute;
+  getAll: GetAllRoute;
   constructor(baseRoute: string) {
     super([baseRoute, 'type']);
 
     this.current = new CurrentRoute(this.baseRoute);
     this.create = new CreateType(this.baseRoute);
+    this.getAll = new GetAllRoute(this.baseRoute);
   }
 }
