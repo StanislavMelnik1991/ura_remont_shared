93c6befb984614eb714ffd92cbb36296671ae2d3
StanislavMelnik1991 <StanislavMelnik@gmail.com>
2024-03-22 22:21:28 +0300

    fix: getRoute

--- routes/admin/dictionary/current/update/index.ts
+++ routes/admin/dictionary/current/update/index.ts
@@ -4,12 +4,11 @@ import scheme from './scheme';
 export class UpdateRoute extends BaseRouter {
   private dictionaryIdMask: string;
   scheme: typeof scheme;
+  getRoute: (id: number) => string;
   constructor(baseRoute: string, dictionaryIdMask: string) {
     super([baseRoute, `:${dictionaryIdMask}`]);
     this.scheme = scheme;
     this.dictionaryIdMask = dictionaryIdMask;
-  }
-  getRoute(id: number) {
-    return this.baseRoute.replace(`:${this.dictionaryIdMask}`, String(id));
+    this.getRoute = (id) => `${baseRoute}/${id}`;
   }
 }
